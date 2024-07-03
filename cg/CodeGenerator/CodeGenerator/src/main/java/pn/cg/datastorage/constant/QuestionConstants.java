1c7baae20626c13f0fde92bc40b1d2a73facadf9
pwgit-create <snow_900@outlook.com>
2024-07-03 18:32:11 +0200

    Updated question
    Signed-off-by: pwgit-create <snow_900@outlook.com>

--- cg/CodeGenerator/CodeGenerator/src/main/java/pn/cg/datastorage/constant/QuestionConstants.java
+++ cg/CodeGenerator/CodeGenerator/src/main/java/pn/cg/datastorage/constant/QuestionConstants.java
@@ -167,7 +167,7 @@ public record QuestionConstants() {
      * @part-of-question-chain WHICH_CLASS_ARE_NEEDED_FOR_APP_PREFIX
      * @chain-order 5/5
      */
-    public final static String CLARIFY_THAT_NO_DOTS_OR_NUMBERS_SHOULD_BE_INCLUDED_IN_THE_RESPONSE="Your response shall not include numbers or dots or spaces to state the order of the classes, it shall only be one class name per line";
+    public final static String CLARIFY_THAT_NO_DOTS_OR_NUMBERS_SHOULD_BE_INCLUDED_IN_THE_RESPONSE="Your response shall only be one class name per line and nothing else. That is very important!";
 
     /**
      * This is the line that initiates the phase that happens after the classes for the super app has been stated
